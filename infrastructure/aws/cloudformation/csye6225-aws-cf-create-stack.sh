d3d1379c4736be7c51f5e28898914035726aa036
pepsiduoduo <345378977@qq.com>
2019-10-10 12:27:37 -0800

    shell script

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,5 +1,5 @@
-echo "Enter your stack name:"
-read name
-echo "your stack name is :" $name
-aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-networking.json --parameters "ParameterKey=StackName,ParameterValue=$name"
+##echo $1
+##read name
+echo "your stack name is :" $1
+aws cloudformation create-stack --stack-name $1 --template-body file://csye6225-cf-networking.json --parameters "ParameterKey=StackName,ParameterValue=o$1"
 
